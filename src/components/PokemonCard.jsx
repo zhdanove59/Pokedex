eae8530672395ab0151120506651f9819f8cd122
zhdanove59 <133035847+zhdanove59@users.noreply.github.com>
2024-04-03 12:04:17 +0200

    component 2 finit

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,9 +1,26 @@
 function PokemonCard() {
-    return <>
-    
-    <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img></figure>;
-    <figcaption>Bulbasaur</figcaption>
+  const pokemon = pokemonList[2]
+    return( <>
+    {pokemon.imgSrc?(<img src={pokemon.imgSrc}/>):(<p>??????</p>)}
+    <figcaption>{pokemon.name}</figcaption>
+
 </>
-  }
+)
+  };
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "pika",
+      imgSrc:
+        "https://i.pinimg.com/736x/5d/6d/23/5d6d23fd7adb44baba20a60c252da339.jpg",
+    },
+    {
+      name: "mew",
+    },
+  ]
   export default PokemonCard;
   
