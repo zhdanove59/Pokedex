a2e264a29d05cc5b344727223343ca6495fb2490
zhdanove59 <133035847+zhdanove59@users.noreply.github.com>
2024-04-08 10:40:59 +0200

    quete numero 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Hello from './component/nav'
 import PokemonCard from './components/PokemonCard'
 import './App.css'
 
 function App() {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "pika",
+      imgSrc:
+        "https://i.etsystatic.com/46611762/r/il/323a26/5653417372/il_794xN.5653417372_12rs.jpg",
+    },
+    {
+      name: "mew",
+    },
+  ]
   return (
     <>
     <div>
       <Hello/>
-      <PokemonCard/>
+      <PokemonCard doudi={pokemonList[0]} />
       </div>
       
     </>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,12 @@
-function PokemonCard() {
-  const pokemon = pokemonList[2]
+function PokemonCard({doudi}){
+  
     return( <>
-    {pokemon.imgSrc?(<img src={pokemon.imgSrc}/>):(<p>??????</p>)}
-    <figcaption>{pokemon.name}</figcaption>
+    {doudi.imgSrc?(<img src={doudi.imgSrc}/>):(<p>??????</p>)}
+    <figcaption>{doudi.name}</figcaption>
 
 </>
-)
-  };
-  const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "pika",
-      imgSrc:
-        "https://i.pinimg.com/736x/5d/6d/23/5d6d23fd7adb44baba20a60c252da339.jpg",
-    },
-    {
-      name: "mew",
-    },
-  ]
+)}
+
+  
   export default PokemonCard;
   
